588fa7f00c44bc15d390dead9dbaabc47c16d08e
ShevaGed <shevaged1990@gmail.com>
2025-12-11 18:09:44 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,103 @@
+"use strict";
+
+//let num = 5;
+//const leftBorderWidth = 1;
+
+//num = 10;
+//console.log(num);
+
+
+//const oba = {
+//    a: 50
+//};
+
+//oba.a = 10;
+
+//console.log(oba);
+
+
+//console.log(name);
+//var name = 'Kit';
+
+//{
+//    let result = 50;
+//}
+
+
+//let number = 4.6;
+
+//console.log(-4/0);
+//console.log('string' * 9);
+
+//const persone = `Anton`;
+
+//const bool = false;
+
+//console.log(Something);
+
+//const obj = {
+//    name: "Persey",
+//    age: 11,
+//    isMarried: false
+//};
+
+//obj.color = "orange";
+//obj["color"] = "orange";
+
+//console.log(obj.name);
+//console.log(obj['color']);
+
+//let arr = ['plum.png', 5, 'orangre.jpg', {}, []];
+//console.log(arr[0]);
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Are you Ok?", "Ok");
+//console.log(typeof(answer));
+
+//const answers = [];
+
+//answers[0] = prompt("What is your name?");
+//answers[1] = prompt("How old are you?");
+//answers[2] = prompt("Do you like my cat?");
+
+//document.write(answers);
+
+//const category = "toys";
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = "Anton";
+
+//alert(`Hello, ${user}`);
+
+//console.log("arr" + " - object");
+//console.log(4 + +"5");
+
+//let incr = 10,
+//    decr = 10;
+
+//incr++;
+//--decr;
+
+//console.log(incr++);
+//console.log(--decr);
+
+//console.log(5%2);
+
+//console.log(2*4 == "8");
+//console.log(2*4 === "8");
+console.log(2 + 2 * 2 != "6");
+console.log(2 + 2 * 2 !== "6");
+
+
+//const isChecked = true,
+//      isClose = false;
+
+//console.log(isChecked && isClose);
+//console.log(isChecked || isClose);
+//console.log(isChecked && !isClose);
+
